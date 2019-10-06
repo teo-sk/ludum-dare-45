060f44c9ac74047eb5003e7667ea2b2eb2d538d9
Matej Zilak <teo@teo.sk>
2019-10-06 10:45:36 +0200

    Add asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Asteroid : MonoBehaviour
+{
+    public GameObject destroyEffect;
+
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var source = GetComponent<AudioSource>();
+        source.PlayOneShot(source.clip);
+        var e = Instantiate(destroyEffect, transform.position, transform.rotation);
+    }
+}
